d6b4142ceae38dd2d33d9e3cc6061368ab9683a3
Your Name <you@example.com>
2025-07-18 00:50:07 +0530

    debug again

--- controllers/authController.js
+++ controllers/authController.js
@@ -13,7 +13,7 @@ exports.login = async (req, res) => {
 
     const token = jwt.sign({ id: user._id, role: user.role }, process.env.JWT_SECRET, { expiresIn: '1d' });
     res.cookie('token', token, { httpOnly: true,  secure: true,       // 👈 required for cross-origin cookies
-  sameSite: 'none', maxAge: 24 * 60 * 60 * 1000}).json({
+  sameSite: 'None', maxAge: 24 * 60 * 60 * 1000}).json({
   message: 'Login successful',
   user: {
     id: user._id,
